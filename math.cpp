f146bd687300a2d77a460c77936deb4d02b49d6f
mcTusa <115863893+mcTusa@users.noreply.github.com>
2022-11-19 18:08:29 +0300

    Add ToFloat()

--- math.cpp
+++ math.cpp
@@ -3,6 +3,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <iostream>
+#include <string>
 #define DEG2RAD(a)(a*M_PI/180)
 #define RAD2DEG(a)(a*180/M_PI)
 int ToInt(float a)
@@ -54,6 +55,62 @@ int ToInt(std::string a)
 	try
 	{
 		int b = std::stoi(a);
+		return b;
+	}
+	catch (...)
+	{
+		return 0;
+	}
+}
+float ToFloat(int a)
+{
+	try
+	{
+		return (float)a;
+	}
+	catch (...)
+	{
+		return 0;
+	}
+}
+float ToFloat(double a)
+{
+	try
+	{
+		return (float)a;
+	}
+	catch (...)
+	{
+		return 0;
+	}
+}
+float ToFloat(char* a)
+{
+	try
+	{
+		return (float)a;
+	}
+	catch (...)
+	{
+		return 0;
+	}
+}
+float ToFloat(char a)
+{
+	try
+	{
+		return (float)a;
+	}
+	catch (...)
+	{
+		return 0;
+	}
+}
+float ToFloat(std::string a)
+{
+	try
+	{
+		float b = std::stof(a);
 		return b;
 	}
 	catch (...)
